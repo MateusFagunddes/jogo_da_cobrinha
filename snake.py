ca68dacbe1bc50b69805fc614949ac7991108730
Mateus Fagundes <87448273+MateusFagunddes@users.noreply.github.com>
2022-04-06 21:00:46 -0300

    Update snake.py

--- snake.py
+++ snake.py
@@ -17,17 +17,17 @@ DOWN = 2
 LEFT = 3
 
 pygame.init()
-screen = pygame.display.set_mode((500, 500))
+screen = pygame.display.set_mode((600, 600))
 pygame.display.set_caption('Jogo da Cobrinha')
 # gerar cobrinha
-snake = [(200, 200), (210, 200), (220,200)]
-snake_skin = pygame.Surface((10,10))
-snake_skin.fill((215,215,215)) #White
+cobrinha = [(200, 200), (210, 200), (220,200)]
+cobrinha_skin = pygame.Surface((10,10))
+cobrinha_skin.fill((215,215,215)) #White
 
 # gerar comidinha
-apple_pos = on_grid_random()
-apple = pygame.Surface((10,10))
-apple.fill((0,150,0))
+comidinha_pos = on_grid_random()
+comidinha = pygame.Surface((10,10))
+comidinha.fill((0,150,0))
 
 my_direction = DOWN
 
@@ -54,53 +54,53 @@ while not game_over:
             if event.key == K_RIGHT and my_direction != LEFT:
                 my_direction = RIGHT
 
-    if collision(snake[0], apple_pos):
-        apple_pos = on_grid_random()
-        snake.append((0,0))
+    if collision(cobrinha[0], comidinha_pos):
+        comidinha_pos = on_grid_random()
+        cobrinha.append((0,0))
         score = score + 1
 
     # checa se a cobrinha bateu nas bordas
-    if snake[0][0] == 500 or snake[0][1] == 500 or snake[0][0] < 0 or snake[0][1] < 0:
+    if cobrinha[0][0] == 600 or cobrinha[0][1] == 600 or cobrinha[0][0] < 0 or cobrinha[0][1] < 0:
         game_over = True
         break
 
     # checa se a cobrinha bateu no próprio corpo
-    for i in range(1, len(snake) - 1):
-        if snake[0][0] == snake[i][0] and snake[0][1] == snake[i][1]:
+    for i in range(1, len(cobrinha) - 1):
+        if cobrinha[0][0] == cobrinha[i][0] and cobrinha[0][1] == cobrinha[i][1]:
             game_over = True
             break
 
     if game_over:
         break
 
-    for i in range(len(snake) - 1, 0, -1):
-        snake[i] = (snake[i-1][0], snake[i-1][1])
+    for i in range(len(cobrinha) - 1, 0, -1):
+        cobrinha[i] = (cobrinha[i-1][0], cobrinha[i-1][1])
 
     # movimentando a cobrinha.
     if my_direction == UP:
-        snake[0] = (snake[0][0], snake[0][1] - 10)
+        cobrinha[0] = (cobrinha[0][0], cobrinha[0][1] - 10)
     if my_direction == DOWN:
-        snake[0] = (snake[0][0], snake[0][1] + 10)
+        cobrinha[0] = (cobrinha[0][0], cobrinha[0][1] + 10)
     if my_direction == RIGHT:
-        snake[0] = (snake[0][0] + 10, snake[0][1])
+        cobrinha[0] = (cobrinha[0][0] + 10, cobrinha[0][1])
     if my_direction == LEFT:
-        snake[0] = (snake[0][0] - 10, snake[0][1])
+        cobrinha[0] = (cobrinha[0][0] - 10, cobrinha[0][1])
 
     screen.fill((50,50,50))
-    screen.blit(apple, apple_pos)
+    screen.blit(comidinha, comidinha_pos)
 
-    for x in range(0, 500, 10): # linhas horizontais
-        pygame.draw.line(screen, (50, 50, 50), (x, 0), (x, 500))
-    for y in range(0, 500, 10): # linhas verticais
-        pygame.draw.line(screen, (50, 50, 50), (0, y), (500, y))
+    for x in range(0, 600, 10): # linhas horizontais
+        pygame.draw.line(screen, (50, 50, 50), (x, 0), (x, 600))
+    for y in range(0, 600, 10): # linhas verticais
+        pygame.draw.line(screen, (50, 50, 50), (0, y), (600, y))
 
     score_font = font.render('Pontos: %s' % (score), True, (255, 255, 255))
     score_rect = score_font.get_rect()
-    score_rect.topleft = (500 - 120, 10)
+    score_rect.topleft = (600 - 120, 10)
     screen.blit(score_font, score_rect)
 
-    for pos in snake:
-        screen.blit(snake_skin,pos)
+    for pos in cobrinha:
+        screen.blit(cobrinha_skin,pos)
 
     pygame.display.update()
 
@@ -108,10 +108,10 @@ while True:
     game_over_font = pygame.font.Font('freesansbold.ttf', 75)
     game_over_screen = game_over_font.render('Game Over', True, (255, 20, 20))
     game_over_rect = game_over_screen.get_rect()
-    game_over_rect.midtop = (500/2, 500/2-75)
+    game_over_rect.midtop = (600/2, 600/2-75)
     screen.blit(game_over_screen, game_over_rect)
     pygame.display.update()
-    pygame.time.wait(500)
+    pygame.time.wait(600)
     while True:
         for event in pygame.event.get():
             if event.type == QUIT:
